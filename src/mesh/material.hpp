5649719eb0f7148766bfb1667ddced71796f48e4
xdgorgola <pedro18fe@gmail.com>
2024-11-07 15:05:17 -0400

    Added shared_ptr to material

--- src/mesh/material.hpp
+++ src/mesh/material.hpp
@@ -1,10 +1,16 @@
 #pragma once
+
+#include <memory>
+
 #include "../../thirdparty/glm/glm.hpp"
 #include "../textures/texture.hpp"
+
+using std::shared_ptr;
 
 /// @brief Struct representing the material properties of a shader
 struct Material
 {
+    shared_ptr<Texture> albedo = nullptr;
     Texture* texture = nullptr;
     glm::vec2 texUVoffset = glm::vec2(0.0f);
     glm::vec2 texUVscales = glm::vec2(1.0f);
